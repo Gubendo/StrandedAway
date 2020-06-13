7301ba793c4f0d51cb67f829eaa6ba1ddcab85b9
Gubendo <37445226+Gubendo@users.noreply.github.com>
2020-06-13 14:41:09 -0400

    initial commit

---
+++ Library/Collab/Base/Assets/Scripts/Interactable/ChangeScene.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class ChangeScene : Interactable
+{
+    public string scene;
+    public override void interact()
+    {
+        SceneManager.LoadScene(scene);
+        if (scene == "Forest")
+        { 
+            SamController.instance.transform.position = new Vector3(-75, -30, 0);
+        }
+        else if (scene == "Camp" && SceneManager.GetActiveScene().name == "Forest")
+        {
+            SamController.instance.transform.position = new Vector3(38.6f, 2.17f, 0);
+        }
+        else if (scene == "Spaceship")
+        {
+            SamController.instance.transform.position = new Vector3(-2.9f, -1.4f, 0);
+        }
+        else if (scene == "Camp" && SceneManager.GetActiveScene().name == "Spaceship")
+        {
+            SamController.instance.transform.position = new Vector3(0.58f, 3.19f, 0);
+        }
+        else if(scene == "Mountain" && SceneManager.GetActiveScene().name == "Camp")
+        {
+            SamController.instance.transform.position = new Vector3(4.55f, -5.0f, 0);
+        }
+        else if (scene == "Camp" && SceneManager.GetActiveScene().name == "Mountain")
+        {
+            SamController.instance.transform.position = new Vector3(5.5f, 43.0f, 0);
+        }
+        else if (scene == "Caverne")
+        {
+            SamController.instance.transform.position = new Vector3(2.49f, 9.37f, 0);
+        }
+        else if (scene == "Camp" && SceneManager.GetActiveScene().name == "Caverne")
+        {
+            SamController.instance.transform.position = new Vector3(-32.51f, 4.26f, 0);
+        }
+        else if (scene == "Village")
+        {
+            SamController.instance.transform.position = new Vector3(0, 24, 0);
+        }
+        else if (scene == "Camp" && SceneManager.GetActiveScene().name == "Village")
+        {
+            SamController.instance.transform.position = new Vector3(1.5f, -35f, 0);
+        }
+
+    }
+}
